9a1de2bffa7c4b49e563af21a2f31ed799a02e70
bunglcph15939 <bunglcph15939@fpt.edu.vn>
2022-04-03 23:56:24 +0700

    asm

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './layout/admin/admin.component'
+import { ProductComponent } from './screens/admin/product/product.component';
+import { ProductFormComponent } from './screens/admin/product-form/product-form.component';
+import { HomeComponent } from './screens/client/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'admin',
+    component:AdminComponent,
+    children:[
+
+      {
+        path :'product',
+        component:ProductComponent
+      },
+      {
+        path :'product/form',
+        component:ProductFormComponent
+      },
+    ]
+
+  },
+  {
+    path :"client",
+    component:HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AdminComponent } from './layout/admin/admin.component';
+import { ProductComponent } from './screens/admin/product/product.component';
+import { ProductFormComponent } from './screens/admin/product-form/product-form.component';
+import { HomeComponent } from './screens/client/home/home.component';
+import { ClientComponent } from './layout/client/client.component';
+import { InfoComponent } from './screens/admin/info/info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    ProductComponent,
+    ProductFormComponent,
+    HomeComponent,
+    ClientComponent,
+    InfoComponent
   ],
   imports: [
     BrowserModule,
